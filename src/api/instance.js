4bdf525265fc2f4ee5db14ab03c7d119aeae6e79
riinavi1 <arina@lambda.direct>
2020-10-17 20:09:20 +0300

    try #10 add log

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const instance = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: `${process.env.BASE_URL}${process.env.BOT_TOKEN}`,
 });
 
 module.exports.instance = instance;
